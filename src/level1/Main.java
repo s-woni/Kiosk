ac210ebdcbebf553c85139f657fa5cf1b2bcf858
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-07 16:37:30 +0900

    Feat: "키오스크 Lv 1 메뉴 출력 기능 구현"

---
+++ src/level1/Main.java
@@ -0,0 +1,19 @@
+package level1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("                                [ STARBUCKS MENU ]");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("1. Americano            | $ 4.5 | 진한 에스프레소에 정수물이 더해진 커피");
+        System.out.println("2. Caffe Latte          | $ 5.0 | 풍부하고 진한 에스프레소에 신선산 스팀 밀크가 더해진 커피");
+        System.out.println("3. Cappuccino           | $ 5.0 | 우유 거품이 풍부한 부드러운 커피");
+        System.out.println("4. Caramel Macchiato    | $ 5.5 | 달콤한 카라멜과 에스프레소가 어우러진 커피");
+
+    }
+}
