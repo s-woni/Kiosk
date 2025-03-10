13453cb1234ccb8ff51c5a0fb60843008ca47091
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-10 15:22:35 +0900

    Feat: "키오스크 Lv 2 메뉴 출력 기능 클래스로 수정"

---
+++ src/level2/MenuItem.java
@@ -0,0 +1,17 @@
+package level2;
+
+public class MenuItem {
+    public String menuName;
+    public double menuPrice;
+    public String menuDescription;
+
+    public MenuItem(String menuName, double menuPrice, String menuDescription) {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuDescription = menuDescription;
+    }
+
+    public void printMenu(int number) {
+        System.out.printf("%d. %-20s | $ %.1f | %s\n", number, this.menuName, this.menuPrice, this.menuDescription);
+    }
+}
