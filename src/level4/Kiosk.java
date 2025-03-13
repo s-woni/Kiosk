518e4d9b701d1b3fe40ab451dec3b47b7a1f72ad
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-13 15:11:40 +0900

    Feat: "키오스크 Lv 4 메뉴 클래스 생성 & 카테고리 추가"

---
+++ src/level4/Kiosk.java
@@ -0,0 +1,123 @@
+package level4;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    private List<Menu> menus;
+
+    public Kiosk(List<Menu> menus) {
+        this.menus = menus;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            printCategoryList();
+
+            try {
+                int categoryNum = scanner.nextInt();
+
+                validateNumber(categoryNum);
+                while(true) {
+                    printMenuList(categoryNum);
+
+                    try {
+                        System.out.print("order : ");
+                        int order = scanner.nextInt();
+
+                        if (order == 9) {
+                            break;
+                        }
+                        validateOrderNumber(categoryNum, order);
+                        printOrder(categoryNum, order);
+                    } catch (InputMismatchException e) {
+                        System.out.println();
+                        System.out.println("잘못된 입력입니다.");
+                        System.out.println("숫자를 입력해주세요.");
+                        scanner.nextLine();
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println();
+                System.out.println("잘못된 입력입니다.");
+                System.out.println("숫자를 입력해주세요.");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public void printCategoryList() {
+        System.out.println();
+        System.out.println("                                [ STARBUCKS MENU ]");
+        System.out.println("--------------------------------------------------------------------------------------");
+        int tempNum = 1;
+        for (Menu item : menus) {
+            item.printCategories(tempNum);
+            tempNum++;
+        }
+        System.out.println("0. Exit                 | 종료");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.print("메뉴를 선택해주세요 : ");
+    }
+
+    public void printMenuList(int categoryNum) {
+        Menu selectedMenu = menus.get(categoryNum - 1);
+        List<MenuItem> menuItems = selectedMenu.getMenuItems();
+        System.out.println();
+        System.out.println("                                [ " + selectedMenu.getCategoryName() + " ]");
+        System.out.println("--------------------------------------------------------------------------------------");
+        int tempNum = 1;
+        for (MenuItem item : menuItems) {
+            item.printMenu(tempNum);
+            tempNum++;
+        }
+        System.out.println("9. Back                 | 뒤로가기");
+        System.out.println("0. Exit                 | 종료");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.print("메뉴를 선택해주세요 : ");
+    }
+
+    public void printOrder(int categoryNum, int order) {
+        Menu selectedMenu = menus.get(categoryNum - 1);
+        MenuItem selectedItem = selectedMenu.getMenuItems().get(order - 1);
+
+        System.out.println("\n" + selectedItem.menuName + "를 주문하셨습니다.\n가격은 $ " + selectedItem.menuPrice + "입니다.");
+    }
+
+    public void validateNumber(int categoryNum) {
+        if (closeKiosk(categoryNum)) {
+            System.exit(0);
+        } else if (categoryNum < 0) {
+            throw new IllegalArgumentException("\n음수를 입력하셨습니다.\n양수를 입력해주세요.");
+        } else if (categoryNum >= menus.size() + 1) {
+            throw new IllegalArgumentException("\n없는 메뉴입니다.\n메뉴에 있는 번호를 골라주세요.");
+        }
+    }
+
+    public void validateOrderNumber(int categoryNum, int order) {
+        Menu selectedMenu = menus.get(categoryNum - 1);
+
+        if (closeKiosk(order)) {
+            System.exit(0);
+        } else if (order < 0) {
+            throw new IllegalArgumentException("\n음수를 입력하셨습니다.\n양수를 입력해주세요.");
+        } else if (order > selectedMenu.getMenuItems().size()) {
+            throw new IllegalArgumentException("\n없는 메뉴입니다.\n메뉴에 있는 번호를 골라주세요.");
+        }
+    }
+
+    public boolean closeKiosk(int order) {
+        if (order == 0) {
+            System.out.println("\n주문을 종료합니다.\n감사합니다!\n");
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/level4/Menu.java
@@ -0,0 +1,30 @@
+package level4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private String categoryName;
+    private List<MenuItem> menuItems;
+
+    public Menu(String categoryName) {
+        this.categoryName = categoryName;
+        this.menuItems = new ArrayList<>();
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return this.menuItems;
+    }
+
+    public String getCategoryName() {
+        return this.categoryName;
+    }
+
+    public void printCategories(int number) {
+        System.out.printf("%d. %s\n", number, categoryName);
+    }
+}
