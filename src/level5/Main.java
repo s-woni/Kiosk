e28df37bb5a1272283c4e65fee15eb534a46ac37
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-13 15:11:59 +0900

    Feat: "키오스크 Lv 5 캡슐화"

---
+++ src/level5/Main.java
@@ -0,0 +1,30 @@
+package level5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Menu drinksMenu = new Menu("Drinks");
+        drinksMenu.addMenuItem(new MenuItem("Americano", 4.5, "진한 에스프레소에 정수물이 더해진 커피"));
+        drinksMenu.addMenuItem(new MenuItem("Caffe Latte", 5.0, "풍부하고 진한 에스프레소에 신선산 스팀 밀크가 더해진 커피"));
+        drinksMenu.addMenuItem(new MenuItem("Cappuccino", 5.0, "우유 거품이 풍부한 부드러운 커피"));
+        drinksMenu.addMenuItem(new MenuItem("Caramel Macchiato", 5.5, "달콤한 카라멜과 에스프레소가 어우러진 커피"));
+
+        Menu cakesMenu = new Menu("Cakes");
+        cakesMenu.addMenuItem((new MenuItem("Chocolate Cake", 5.5, "달콤한 초콜릿 케이크")));
+
+        Menu sandwichsMenu = new Menu("Sandwichs");
+        sandwichsMenu.addMenuItem((new MenuItem("Egg Sandwich", 5.0, "달걀이 들어간 샌드위치")));
+
+        List<Menu> menus = new ArrayList<>();
+        menus.add(drinksMenu);
+        menus.add(cakesMenu);
+        menus.add(sandwichsMenu);
+
+        Kiosk kiosk = new Kiosk(menus);
+
+        kiosk.start();
+    }
+}
---
+++ src/level5/MenuItem.java
@@ -0,0 +1,29 @@
+package level5;
+
+public class MenuItem {
+    private String menuName;
+    private double menuPrice;
+    private String menuDescription;
+
+    public MenuItem(String menuName, double menuPrice, String menuDescription) {
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.menuDescription = menuDescription;
+    }
+
+    public void printMenu(int number) {
+        System.out.printf("%d. %-20s | $ %.1f | %s\n", number, this.menuName, this.menuPrice, this.menuDescription);
+    }
+
+    public double getMenuPrice() {
+        return this.menuPrice;
+    }
+
+    public String getMenuName() {
+        return this.menuName;
+    }
+
+    public String getMenuDescription() {
+        return this.menuDescription;
+    }
+}
