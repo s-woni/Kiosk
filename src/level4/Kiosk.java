c9356bc581ebbca64ce0d7ec5f46627588ae538c
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-14 12:11:02 +0900

    Comment: "Lv 4 주석 추가"

--- src/level4/Kiosk.java
+++ src/level4/Kiosk.java
@@ -7,32 +7,44 @@ import java.util.Scanner;
 public class Kiosk {
     private List<Menu> menus;
 
+    // 생성자
+    // menus 리스트를 받아 추기화
     public Kiosk(List<Menu> menus) {
         this.menus = menus;
     }
 
+    // 키오스크 시작 메서드
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
+            // 카테고리 출력
             printCategoryList();
 
             try {
+                // 사용자 입력
                 int categoryNum = scanner.nextInt();
 
+                // 카테고리 번호 유효 검사
                 validateNumber(categoryNum);
                 while(true) {
+                    // 카테고리에 따른 메뉴 출력
                     printMenuList(categoryNum);
 
                     try {
-                        System.out.print("order : ");
+                        // 사용자 입력
                         int order = scanner.nextInt();
 
+                        // 9 입력 시 카테고리 화면으로 돌아감
                         if (order == 9) {
                             break;
                         }
+                        
+                        // 메뉴 번호 유효성 검사
                         validateOrderNumber(categoryNum, order);
+                        // 주문 정보 출력
                         printOrder(categoryNum, order);
+                    // 숫자가 아닌 다른 값 입력 시 예외 처리    
                     } catch (InputMismatchException e) {
                         System.out.println();
                         System.out.println("잘못된 입력입니다.");
@@ -53,11 +65,13 @@ public class Kiosk {
         }
     }
 
+    // 카테고리 출력 메서드
     public void printCategoryList() {
         System.out.println();
         System.out.println("                                [ STARBUCKS MENU ]");
         System.out.println("--------------------------------------------------------------------------------------");
         int tempNum = 1;
+        // 각 카테고리 출력
         for (Menu item : menus) {
             item.printCategories(tempNum);
             tempNum++;
@@ -67,6 +81,7 @@ public class Kiosk {
         System.out.print("메뉴를 선택해주세요 : ");
     }
 
+    // 메뉴 출력 메서드
     public void printMenuList(int categoryNum) {
         Menu selectedMenu = menus.get(categoryNum - 1);
         List<MenuItem> menuItems = selectedMenu.getMenuItems();
@@ -74,6 +89,7 @@ public class Kiosk {
         System.out.println("                                [ " + selectedMenu.getCategoryName() + " ]");
         System.out.println("--------------------------------------------------------------------------------------");
         int tempNum = 1;
+        // 각 메뉴 출력
         for (MenuItem item : menuItems) {
             item.printMenu(tempNum);
             tempNum++;
@@ -84,13 +100,17 @@ public class Kiosk {
         System.out.print("메뉴를 선택해주세요 : ");
     }
 
+    // 주문 정보 출력 메서드
     public void printOrder(int categoryNum, int order) {
+        // 사용자가 입력한 번호에 대한 카테고리 정보 가져옴
         Menu selectedMenu = menus.get(categoryNum - 1);
+        // 사용자가 고른 카테고리 내부의 메뉴에 대한 정보 가져옴
         MenuItem selectedItem = selectedMenu.getMenuItems().get(order - 1);
 
         System.out.println("\n" + selectedItem.menuName + "를 주문하셨습니다.\n가격은 $ " + selectedItem.menuPrice + "입니다.");
     }
 
+    // 입력한 카테고리 번호가 유효한지 검사하는 메서드
     public void validateNumber(int categoryNum) {
         if (closeKiosk(categoryNum)) {
             System.exit(0);
@@ -101,6 +121,7 @@ public class Kiosk {
         }
     }
 
+    // 입력한 주문 번호가 유효한지 검사하는 메서드
     public void validateOrderNumber(int categoryNum, int order) {
         Menu selectedMenu = menus.get(categoryNum - 1);
 
@@ -113,7 +134,9 @@ public class Kiosk {
         }
     }
 
+    // 키오스크 종료 메서드
     public boolean closeKiosk(int order) {
+        // 0 이 들어오면 키오스크 프로그램 종료
         if (order == 0) {
             System.out.println("\n주문을 종료합니다.\n감사합니다!\n");
             return true;
--- src/level4/Menu.java
+++ src/level4/Menu.java
@@ -7,23 +7,29 @@ public class Menu {
     private String categoryName;
     private List<MenuItem> menuItems;
 
+    // 생성자
+    // 카테고리 이름을 받아 초기화
     public Menu(String categoryName) {
         this.categoryName = categoryName;
-        this.menuItems = new ArrayList<>();
+        this.menuItems = new ArrayList<>(); // 빈 리스트로 초기화
     }
 
+    // 메뉴 아이템 추가 메서드
     public void addMenuItem(MenuItem menuItem) {
         menuItems.add(menuItem);
     }
 
+    // 메뉴 아이템 리스트 반환 메서드
     public List<MenuItem> getMenuItems() {
         return this.menuItems;
     }
 
+    // 카테고리 이름 반환 메서드
     public String getCategoryName() {
         return this.categoryName;
     }
 
+    // 카테고리 출력 메서드
     public void printCategories(int number) {
         System.out.printf("%d. %s\n", number, categoryName);
     }
