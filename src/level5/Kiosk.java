0406f9b5b6f4a4161d7ed8d9674b55ee9dc7cfe0
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-14 12:16:32 +0900

    Comment: "Lv 5 주석 추가"

--- src/level5/Kiosk.java
+++ src/level5/Kiosk.java
@@ -7,31 +7,44 @@ import java.util.Scanner;
 public class Kiosk {
     private List<Menu> menus;
 
+    // 생성자
+    // menus 리스트를 받아 추기화
     public Kiosk(List<Menu> menus) {
         this.menus = menus;
     }
 
+    // 키오스크 시작 메서드
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
+            // 카테고리 출력
             printCategoryList();
 
             try {
+                // 사용자 입력
                 int categoryNum = scanner.nextInt();
 
-                getCategoryNumber(categoryNum);
+                // 카테고리 번호 유효 검사
+                checkedCategoryNumber(categoryNum);
                 while(true) {
+                    // 카테고리에 따른 메뉴 출력
                     printMenuList(categoryNum);
 
                     try {
+                        // 사용자 입력
                         int order = scanner.nextInt();
 
+                        // 9 입력 시 카테고리 화면으로 돌아감
                         if (order == 9) {
                             break;
                         }
-                        getOrderNumber(categoryNum, order);
+
+                        // 메뉴 번호 유효성 검사
+                        checkedOrderNumber(categoryNum, order);
+                        // 주문 정보 출력
                         printOrder(categoryNum, order);
+                    // 숫자가 아닌 다른 값 입력 시 예외 처리
                     } catch (InputMismatchException e) {
                         System.out.println();
                         System.out.println("잘못된 입력입니다.");
@@ -52,11 +65,13 @@ public class Kiosk {
         }
     }
 
+    // 카테고리 출력 메서드
     public void printCategoryList() {
         System.out.println();
         System.out.println("                                [ STARBUCKS MENU ]");
         System.out.println("--------------------------------------------------------------------------------------");
         int tempNum = 1;
+        // 각 카테고리 출력
         for (Menu item : menus) {
             item.printCategories(tempNum);
             tempNum++;
@@ -66,6 +81,7 @@ public class Kiosk {
         System.out.print("메뉴를 선택해주세요 : ");
     }
 
+    // 메뉴 출력 메서드
     public void printMenuList(int categoryNum) {
         Menu selectedMenu = menus.get(categoryNum - 1);
         List<MenuItem> menuItems = selectedMenu.getMenuItems();
@@ -73,6 +89,7 @@ public class Kiosk {
         System.out.println("                                [ " + selectedMenu.getCategoryName() + " ]");
         System.out.println("--------------------------------------------------------------------------------------");
         int tempNum = 1;
+        // 각 메뉴 출력
         for (MenuItem item : menuItems) {
             item.printMenu(tempNum);
             tempNum++;
@@ -83,22 +100,28 @@ public class Kiosk {
         System.out.print("메뉴를 선택해주세요 : ");
     }
 
+    // 주문 정보 출력 메서드
     public void printOrder(int categoryNum, int order) {
+        // 사용자가 입력한 번호에 대한 카테고리 정보 가져옴
         Menu selectedMenu = menus.get(categoryNum - 1);
+        // 사용자가 고른 카테고리 내부의 메뉴에 대한 정보 가져옴
         MenuItem selectedItem = selectedMenu.getMenuItems().get(order - 1);
 
         System.out.println("\n" + selectedItem.getMenuName() + "를 주문하셨습니다.\n가격은 $ " + selectedItem.getMenuPrice() + "입니다.");
     }
 
-    public void getCategoryNumber(int categoryNum) {
+    // 입력한 카테고리 번호가 유효한지 확인하는 메서드
+    public void checkedCategoryNumber(int categoryNum) {
         validateNumber(categoryNum, menus.size());
     }
 
-    public void getOrderNumber(int categoryNum, int order) {
+    // 입력한 주문 번호가 유효한지 확인하는 메서드
+    public void checkedOrderNumber(int categoryNum, int order) {
         Menu selectedMenu = menus.get(categoryNum - 1);
         validateNumber(order, selectedMenu.getMenuItems().size());
     }
 
+    // 입력한 번호와 리스트의 크기를 가져와 유효 검사 메서드
     public void validateNumber(int number, int maxSize) {
         if (closeKiosk(number)) {
             System.exit(0);
@@ -109,7 +132,9 @@ public class Kiosk {
         }
     }
 
+    // 키오스크 종료 메서드
     public boolean closeKiosk(int order) {
+        // 0 이 들어오면 키오스크 프로그램 종료
         if (order == 0) {
             System.out.println("\n주문을 종료합니다.\n감사합니다!\n");
             return true;
--- src/level5/MenuItem.java
+++ src/level5/MenuItem.java
@@ -5,24 +5,30 @@ public class MenuItem {
     private double menuPrice;
     private String menuDescription;
 
+    // 생성자
+    // 메뉴이름, 가격, 설명 초기화
     public MenuItem(String menuName, double menuPrice, String menuDescription) {
         this.menuName = menuName;
         this.menuPrice = menuPrice;
         this.menuDescription = menuDescription;
     }
 
+    // 메뉴 정보를 출력
     public void printMenu(int number) {
         System.out.printf("%d. %-20s | $ %.1f | %s\n", number, this.menuName, this.menuPrice, this.menuDescription);
     }
 
+    // 메뉴 가격을 반환하는 메서드
     public double getMenuPrice() {
         return this.menuPrice;
     }
 
+    // 메뉴 이름을 반환하는 메서드
     public String getMenuName() {
         return this.menuName;
     }
 
+    // 메뉴 설명을 반환하는 메서드
     public String getMenuDescription() {
         return this.menuDescription;
     }
