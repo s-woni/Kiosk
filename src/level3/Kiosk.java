8ae4b291e8d9d348d54eb676426ae354e4af1084
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-14 12:01:05 +0900

    Comment: "Lv 3 주석 추가"

--- src/level3/Kiosk.java
+++ src/level3/Kiosk.java
@@ -7,25 +7,34 @@ import java.util.Scanner;
 public class Kiosk {
     private List<MenuItem> menuItems;
 
+    // 생성자
+    // menuItems 리스트를 받아 초기화
     public Kiosk(List<MenuItem> menuItems) {
         this.menuItems = menuItems;
     }
 
+    // 키오스크 시작 메서드
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
+            // 메뉴 출력
             printMenuList();
 
             try {
+                // 사용자 입력
                 int order = scanner.nextInt();
 
+                // 0 입력시 프로그램 종료
                 if (closeKiosk(order)) {
                     return;
                 }
+                // 입력 값 유효 검사
                 validateNumber(order);
+                // 주문정보 출력
                 printOrder(order);
-
+            
+            // 숫자가 아닌 다른 값 입력 시 예외 처리    
             } catch (InputMismatchException e) {
                 System.out.println();
                 System.out.println("잘못된 입력입니다.");
@@ -37,11 +46,13 @@ public class Kiosk {
         }
     }
 
+    // 메뉴 출력 메서드
     public void printMenuList() {
         System.out.println();
         System.out.println("                                [ STARBUCKS MENU ]");
         System.out.println("--------------------------------------------------------------------------------------");
         int tempNum = 1;
+        // 메뉴판 내부 메뉴 출력
         for (MenuItem item : menuItems) {
             item.printMenu(tempNum);
             tempNum++;
@@ -51,12 +62,15 @@ public class Kiosk {
         System.out.print("메뉴를 선택해주세요 : ");
     }
 
+    // 주문 정보 출력 메서드
     public void printOrder(int order) {
+        // 사용자가 입력한 번호에 해당하는 메뉴를 가져옴
         MenuItem selectedItem = menuItems.get(order - 1);
 
         System.out.println("\n" + selectedItem.menuName + "를 주문하셨습니다.\n가격은 $ " + selectedItem.menuPrice + "입니다.");
     }
 
+    // 입력한 값이 유효한지 검사하는 메서드
     public void validateNumber(int order) {
         if (order < 0) {
             throw new IllegalArgumentException("\n음수를 입력하셨습니다.\n양수를 입력해주세요.");
@@ -65,7 +79,9 @@ public class Kiosk {
         }
     }
 
+    // 키오스크 종료 메서드
     public boolean closeKiosk(int order) {
+        // 0 이 들어오면 키오스크 프로그램 종료
         if (order == 0) {
             System.out.println("\n주문을 종료합니다.\n감사합니다!\n");
             return true;
--- src/level3/MenuItem.java
+++ src/level3/MenuItem.java
@@ -5,12 +5,15 @@ public class MenuItem {
     public double menuPrice;
     public String menuDescription;
 
+    // 생성자
+    // 메뉴이름, 가격, 설명 초기화
     public MenuItem(String menuName, double menuPrice, String menuDescription) {
         this.menuName = menuName;
         this.menuPrice = menuPrice;
         this.menuDescription = menuDescription;
     }
 
+    // 메뉴 정보를 출력
     public void printMenu(int number) {
         System.out.printf("%d. %-20s | $ %.1f | %s\n", number, this.menuName, this.menuPrice, this.menuDescription);
     }
